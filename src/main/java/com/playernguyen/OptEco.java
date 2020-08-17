1453e0785317855d0cff84dfcc426a35dadebc03
nguyen <daudaua3782@gmail.com>
2020-08-17 21:02:31 +0700

    Initial commit

---
+++ src/main/java/com/playernguyen/OptEco.java
@@ -0,0 +1,11 @@
+package com.playernguyen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OptEco extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //TODO: do the works
+    }
+}
